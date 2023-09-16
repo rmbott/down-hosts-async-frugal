113a3d42972930008a0c17d361ebe0d00a12f670
Ryan Bott <tmp8496@gmail.com>
2023-09-16 08:21:20 -0400

    typo in comments

--- downhosts.py
+++ downhosts.py
@@ -2,7 +2,7 @@ import time
 from asyncping3 import ping
 import asyncio
 
-# Takes a list of IPs and returns a list of IPs addresses that are down.
+# Takes a list of IPs and returns a list of IPs that are down.
 # Pings are done in asyncronous batches. If all hosts are up, the number of 
 # batches = 1. If one or more hosts are down, the number of 
 # batches = "batches". Unless all down hosts turn out to be up on retry. In 
@@ -11,11 +11,11 @@ import asyncio
 async def tf_ping(host:str) -> bool:
     response = await ping(host)
     if type(response) is float:
-        return True # async3.ping returns responsetime floats for successful pings
+        return True # asyncping3.ping returns responsetime floats for successful pings
     if response is None:
-        return False # async3.ping returns None for timeouts
+        return False # asyncping3.ping returns None for timeouts
     if response is False:
-        return response  # async3.ping returns False for resolution errors
+        return response  # asyncping3.ping returns False for resolution errors
 
 async def batch(hosts:list)-> list:
     if len(hosts) <= 0:
